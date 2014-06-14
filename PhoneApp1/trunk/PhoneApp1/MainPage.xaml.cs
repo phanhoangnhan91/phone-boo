29db293365ce1002a990d9699d5662c152433fc3
phanhoangnhan91@gmail.com <phanhoangnhan91@gmail.com@ce10e2d4-d70a-6057-643d-3524ab33d3f5>
2014-06-14 16:16:14 +0000

    dau tien

---
+++ PhoneApp1/trunk/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+using Microsoft.Phone.UserData;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+
+            //BuildLocalizedApplicationBar();
+            Contacts cons = new Contacts();
+
+            //Identify the method that runs after the asynchronous search completes.
+            cons.SearchCompleted += new EventHandler<ContactsSearchEventArgs>(Contacts_SearchCompleted);
+
+            //Start the asynchronous search.
+            cons.SearchAsync(String.Empty, FilterKind.None, "Contacts Test #1");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contacts cons = new Contacts();
+
+            //Identify the method that runs after the asynchronous search completes.
+            cons.SearchCompleted += new EventHandler<ContactsSearchEventArgs>(Contacts_SearchCompleted);
+
+            //Start the asynchronous search.
+            cons.SearchAsync(String.Empty, FilterKind.None, "Contacts Test #1");
+        }
+
+        void Contacts_SearchCompleted(object sender, ContactsSearchEventArgs e)
+        {
+            try
+            {
+                //Bind the results to the user interface.
+                ContactResultsData.DataContext = e.Results;
+                
+            }
+            catch (System.Exception)
+            {
+                //No results
+            }
+
+            if (ContactResultsData.Items.Any())
+            {
+                ContactResultsLabel.Text = "results";
+            }
+            else
+            {
+                ContactResultsLabel.Text = "no results";
+            }
+
+        }
+
+        private void ContactResultsData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
